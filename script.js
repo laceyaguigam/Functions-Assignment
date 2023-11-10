43456d28529c154c0679b0eb45572636a78cb3a6
lacey aguigam <laceyaguigam@yahoo.com>
2023-11-09 20:43:11 -0800

    completed the second task, used Math.PI to find the circumference c=2(pi)r

--- script.js
+++ script.js
@@ -3,9 +3,26 @@
 // console.log(min(0, 10)); // will output 0.
 // console.log(min(0, -10)); // will output -10
 
+
+
+
+
+
+
+
 // 2. Write a function that takes the radius of a circle and outputs it’s approximate circumference (the formula for circumference is 2 *pi * radius, you may use 3.14 for pi).
 // Example Output:
 // console.log(calculateCircumference(5)); // will output 31.4
+
+
+function findCircumference(radius) {
+    return 2 * Math.PI * radius;
+}
+console.log(findCircumference(10));  //outputs 62.83
+
+
+
+
 
 // 3. Write a function searchString that takes a character and then returns how many times it appears in another string argument.
 // Example Output:
